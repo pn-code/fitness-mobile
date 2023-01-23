ad6aa6b5665304950b007aaa06085897883ecd5f
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-23 14:39:33 -0800

    Initial React-Native Setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fitness-mobile
+
 Fitness App created with React Native intended to track workouts and calories, and create workout and diet plans.
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCheMUafyCUYueUDqYsBL7vUP_4-JOUB3w",
+  authDomain: "fitness-mobile-1e0cb.firebaseapp.com",
+  projectId: "fitness-mobile-1e0cb",
+  storageBucket: "fitness-mobile-1e0cb.appspot.com",
+  messagingSenderId: "771420981576",
+  appId: "1:771420981576:web:41bce6ec84ee9723f44b7d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
