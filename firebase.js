0f9fce50d8c18b6742bf93077dc8479413d249d9
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-23 21:26:44 -0800

    Firebase Setup with Expo

--- firebase.js
+++ firebase.js
@@ -1,17 +1,27 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCheMUafyCUYueUDqYsBL7vUP_4-JOUB3w",
-  authDomain: "fitness-mobile-1e0cb.firebaseapp.com",
-  projectId: "fitness-mobile-1e0cb",
-  storageBucket: "fitness-mobile-1e0cb.appspot.com",
-  messagingSenderId: "771420981576",
-  appId: "1:771420981576:web:41bce6ec84ee9723f44b7d"
+    apiKey: "AIzaSyCheMUafyCUYueUDqYsBL7vUP_4-JOUB3w",
+    authDomain: "fitness-mobile-1e0cb.firebaseapp.com",
+    projectId: "fitness-mobile-1e0cb",
+    storageBucket: "fitness-mobile-1e0cb.appspot.com",
+    messagingSenderId: "771420981576",
+    appId: "1:771420981576:web:41bce6ec84ee9723f44b7d",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+let app;
+
+if (firebase.apps.length === 0) {
+    // Initialize Firebase
+    app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
