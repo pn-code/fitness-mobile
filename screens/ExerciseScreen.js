f3022e27f02ba203e8e058e5ccabc669325f6176
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-24 19:43:33 -0800

    Added Inputs and Text

--- screens/ExerciseScreen.js
+++ screens/ExerciseScreen.js
@@ -1,12 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { Button, Input, ListItem } from "@rneui/base";
 
 const ExerciseScreen = () => {
-    return (
-        <View>
-            <Text>ExerciseScreen</Text>
-        </View>
-    );
+  const [exercise, setExercise] = useState("");
+  const [sets, setSets] = useState("");
+  const [reps, setReps] = useState("");
+  const [exercises, setExercises] = useState([])
+
+  return (
+    <View>
+      <View style={styles.modal}>
+        <Input placeholder="Exercise Name" />
+        <Input placeholder="Sets" />
+        <Input placeholder="Reps" />
+      </View>
+      <Button onPress={() => setOpen(true)} title="Add Exercise" />
+      <View>
+        {/* Rendering Daily Entries Here*/}
+        <ScrollView>
+          <Text>Date</Text>
+          {/* Rendering Exercise Entries Here */}
+          <View>
+            <Text>Exercise Name</Text>
+            <Text>Sets</Text>
+            <Text>Reps</Text>
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  );
 };
 
 export default ExerciseScreen;
