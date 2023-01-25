5d0d3d28623d0c8c78029c9fa3d7f10b116d5292
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-25 12:04:42 -0800

    Added User Avatar on Header

--- components/HomeTabs.js
+++ components/HomeTabs.js
@@ -1,31 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ExerciseScreen from "../screens/ExerciseScreen";
 import NutritionScreen from "../screens/NutritionScreen";
 import FAQScreen from "../screens/FAQScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import { Avatar } from "@rneui/base";
+import { auth } from "../firebase/firebase";
 
 const HomeTabs = () => {
-  const Tab = createBottomTabNavigator();
-  const options = {
-    headerStyle: { backgroundColor: "black" },
-    headerTitleStyle: { color: "white" },
-    headerTintColor: "white",
-    tabBarActiveTintColor: "white",
-    tabBarInactiveTintColor: "white",
-    tabBarActiveBackgroundColor: "gray",
-    tabBarInactiveBackgroundColor: "black",
-  };
+    const Tab = createBottomTabNavigator();
 
-  return (
-    <Tab.Navigator screenOptions={options}>
-      <Tab.Screen name="Exercise" component={ExerciseScreen} />
-      <Tab.Screen name="Nutrition" component={NutritionScreen} />
-      <Tab.Screen name="FAQ" component={FAQScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-    </Tab.Navigator>
-  );
+    const options = {
+        headerStyle: { backgroundColor: "black" },
+        headerTitleStyle: { color: "white" },
+        headerTintColor: "white",
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarActiveBackgroundColor: "gray",
+        tabBarInactiveBackgroundColor: "black",
+        headerRight: () => (
+            <View>
+                <TouchableOpacity style={{ marginRight: 10 }}>
+                    <Avatar source={{ uri: auth?.currentUser?.photoURL }} />
+                </TouchableOpacity>
+            </View>
+        ),
+    };
+
+    return (
+        <Tab.Navigator screenOptions={options}>
+            <Tab.Screen name="Exercise" component={ExerciseScreen} />
+            <Tab.Screen name="Nutrition" component={NutritionScreen} />
+            <Tab.Screen name="FAQ" component={FAQScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+    );
 };
 
 export default HomeTabs;
