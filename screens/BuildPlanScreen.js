114199ad4125ef3d64fbb932a548d3d2cc7d09e3
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-25 19:04:14 -0800

    Added onChangeText handlers and input states

--- screens/BuildPlanScreen.js
+++ screens/BuildPlanScreen.js
@@ -1,21 +1,53 @@
 import { StyleSheet, Text, ScrollView, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Button, Input } from "@rneui/base";
 
 const BuildPlanScreen = () => {
+    const [title, setTitle] = useState("");
+    const [exercise, setExercise] = useState({
+        name: "",
+        sets: "",
+        reps: "",
+    });
+
+    const handleChange = (text, name) => {
+        setExercise((exercise) => ({ ...exercise, [name]: text }));
+    };
+
     return (
         <ScrollView>
-            <Input placeholder="Plan Name" keyboardType="text" />
-
+            <Input
+                onChangeText={(text) => setTitle(text)}
+                placeholder="Plan Title"
+                keyboardType="text"
+                value={title}
+            />
 
             <View>
-                <Input placeholder="Exercise Name" keyboardType="text" />
-                <Input placeholder="Desired Set Scheme" keyboardType="text" />
-                <Input placeholder="Desired Rep Scheme" keyboardType="text" />
+                <Input
+                    onChangeText={(text) => handleChange(text, "name")}
+                    placeholder="Exercise Name"
+                    keyboardType="text"
+                    value={exercise.name}
+                />
+                <Input
+                    onChangeText={(text) => handleChange(text, "sets")}
+                    placeholder="Set Scheme (for example: 3-4)"
+                    keyboardType="text"
+                    value={exercise.sets}
+                />
+                <Input
+                    onChangeText={(text) => handleChange(text, "reps")}
+                    placeholder="Rep Scheme (for example: 8-12)"
+                    keyboardType="text"
+                    value={exercise.reps}
+                />
             </View>
 
+            <ScrollView>{/* Render ExerciseCards Here */}</ScrollView>
+
             <Button title="Add Exercise" />
-            <Button title="Submit Plan"/>
+            <Button title="Submit Plan" />
         </ScrollView>
     );
 };
