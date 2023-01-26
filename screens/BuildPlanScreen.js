f7d20a1a6e1377b7ac9acb2f0e9ee36ed988d2ae
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-25 18:51:20 -0800

    Added Inputs and Buttons

--- screens/BuildPlanScreen.js
+++ screens/BuildPlanScreen.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, ScrollView, View } from "react-native";
+import React from "react";
+import { Button, Input } from "@rneui/base";
 
 const BuildPlanScreen = () => {
-  return (
-    <View>
-      <Text>BuildPlanScreen</Text>
-    </View>
-  )
-}
+    return (
+        <ScrollView>
+            <Input placeholder="Plan Name" keyboardType="text" />
 
-export default BuildPlanScreen
 
-const styles = StyleSheet.create({})
+            <View>
+                <Input placeholder="Exercise Name" keyboardType="text" />
+                <Input placeholder="Desired Set Scheme" keyboardType="text" />
+                <Input placeholder="Desired Rep Scheme" keyboardType="text" />
+            </View>
+
+            <Button title="Add Exercise" />
+            <Button title="Submit Plan"/>
+        </ScrollView>
+    );
+};
+
+export default BuildPlanScreen;
+
+const styles = StyleSheet.create({});
