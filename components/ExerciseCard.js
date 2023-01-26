23b00ff4635373c282a5e0e536c5a7958f789d64
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-25 19:13:21 -0800

    Replaced hard-coded text with props

--- components/ExerciseCard.js
+++ components/ExerciseCard.js
@@ -2,12 +2,12 @@ import { StyleSheet, View } from "react-native";
 import React from "react";
 import { Text } from "@rneui/base";
 
-const ExerciseCard = () => {
+const ExerciseCard = ({ exercise }) => {
     return (
         <View>
-            <Text>Exercise Name</Text>
-            <Text>Exercise Set Scheme</Text>
-            <Text>Exercise Rep Scheme</Text>
+            <Text>{exercise.name}</Text>
+            <Text>{exercise.sets}</Text>
+            <Text>{exercise.reps}</Text>
         </View>
     );
 };
