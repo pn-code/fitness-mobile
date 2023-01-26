680dff304039ced6567f8561a4287612d6c22581
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-25 18:51:09 -0800

    Initial Commit

---
+++ components/ExerciseCard.js
@@ -0,0 +1,17 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { Text } from "@rneui/base";
+
+const ExerciseCard = () => {
+    return (
+        <View>
+            <Text>Exercise Name</Text>
+            <Text>Exercise Set Scheme</Text>
+            <Text>Exercise Rep Scheme</Text>
+        </View>
+    );
+};
+
+export default ExerciseCard;
+
+const styles = StyleSheet.create({});
