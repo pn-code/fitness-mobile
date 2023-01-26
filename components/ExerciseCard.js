1ca18b10b633670f687eab4e9444cc4f4243acc1
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-25 19:58:07 -0800

    Added Styles

--- components/ExerciseCard.js
+++ components/ExerciseCard.js
@@ -4,14 +4,25 @@ import { Text } from "@rneui/base";
 
 const ExerciseCard = ({ exercise }) => {
     return (
-        <View>
-            <Text>{exercise.name}</Text>
-            <Text>{exercise.sets}</Text>
-            <Text>{exercise.reps}</Text>
+        <View style={styles.container}>
+            <Text style={styles.name}>{exercise.name}</Text>
+            <Text style={styles.detail}>
+                {exercise.sets} sets of {exercise.reps} reps
+            </Text>
         </View>
     );
 };
 
 export default ExerciseCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+    },
+    name: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    detail: {
+        fontSize: 18,
+    },
+});
--- components/PlanCard.js
+++ components/PlanCard.js
@@ -4,8 +4,8 @@ import ExerciseCard from "./ExerciseCard";
 
 const PlanCard = ({ plan }) => {
     return (
-        <View>
-            <Text>{plan.title}</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>{plan.title}</Text>
             {plan.exercises.map((exercise) => (
                 <ExerciseCard exercise={exercise} />
             ))}
@@ -15,4 +15,15 @@ const PlanCard = ({ plan }) => {
 
 export default PlanCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "light-gray",
+        padding: 12,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 8,
+        color: "blue"
+    },
+});
