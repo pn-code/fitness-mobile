a49dfbd7153fc93d08d3121952020a4ff7f30a0c
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-27 18:14:17 -0800

    Initial Commit to fill out LearnStack

---
+++ components/SubjectCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SubjectCard = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default SubjectCard
+
+const styles = StyleSheet.create({})
---
+++ screens/LearnNutritionScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LearnNutritionScreen = () => {
+  return (
+    <View>
+      <Text>LearnNutritionScreen</Text>
+    </View>
+  )
+}
+
+export default LearnNutritionScreen
+
+const styles = StyleSheet.create({})
