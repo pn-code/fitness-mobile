9c489092709a79c24d756b5c0917cb6013a1f1ae
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-27 18:18:53 -0800

    Added Back Navigation handlers

--- screens/LearnFitnessScreen.js
+++ screens/LearnFitnessScreen.js
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Button } from "@rneui/base";
 
-const LearnFitnessScreen = () => {
-  return (
-    <View>
-      <Text>LearnFitnessScreen</Text>
-    </View>
-  )
-}
+const LearnFitnessScreen = ({ navigation }) => {
+    return (
+        <View>
+            <Button
+                onPress={() => navigation.navigate("Home")}
+                title="Back"
+            />
+            <Text>LearnFitnessScreen</Text>
+        </View>
+    );
+};
 
-export default LearnFitnessScreen
+export default LearnFitnessScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- screens/LearnNutritionScreen.js
+++ screens/LearnNutritionScreen.js
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Button } from "@rneui/base";
 
-const LearnNutritionScreen = () => {
-  return (
-    <View>
-      <Text>LearnNutritionScreen</Text>
-    </View>
-  )
-}
+const LearnNutritionScreen = ({ navigation }) => {
+    return (
+        <View>
+            <Button
+                onPress={() => navigation.navigate("Home")}
+                title="Back"
+            />
+            <Text>LearnNutritionScreen</Text>
+        </View>
+    );
+};
 
-export default LearnNutritionScreen
+export default LearnNutritionScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
