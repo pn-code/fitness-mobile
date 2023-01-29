ae765267042b17a279cfed6666f8a6379e599ce1
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-28 22:11:05 -0800

    Added header, desc, and log out btn

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Button, Image } from "@rneui/base";
+import { auth } from "../firebase/firebase";
 
-const ProfileScreen = () => {
+const ProfileScreen = ({ navigation }) => {
+  const { currentUser: user } = auth;
+  console.log(user);
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace("Login");
+    });
+  };
+
   return (
     <View>
-      <Text>ProfileScreen</Text>
+      {/* Profile Header */}
+      <View style={styles.header}>
+        <Image
+          style={{ width: 80, height: 80, borderRadius: 50 }}
+          source={{ uri: user.photoURL }}
+        />
+        <Text style={styles.headerText}>{user.displayName}</Text>
+      </View>
+
+      {/* Description */}
+      <View style={styles.desc}>
+        <Text>Description</Text>
+      </View>
+
+      {/* User Log Out */}
+      <View>
+        <Button onPress={signOutUser} title="Log Out" />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  desc: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+});
