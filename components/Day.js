c0fad962b855c4d2a97c2456ff1df41f248c0d71
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-30 21:11:57 -0800

    Passed id and edit props to Day component

--- components/Day.js
+++ components/Day.js
@@ -3,7 +3,7 @@ import { Text } from "@rneui/base";
 import React from "react";
 import Exercise from "./Exercise";
 
-const Day = ({ date, exercises }) => {
+const Day = ({ date, exercises, deleteExercise, edit }) => {
     return (
         <ScrollView style={styles.container}>
             <Text h4 style={styles.date}>
@@ -17,10 +17,14 @@ const Day = ({ date, exercises }) => {
                         exercise.sets +
                         exercise.reps
                     }
+                    id={exercise.id}
+                    date={date}
                     name={exercise.name}
                     weight={exercise.weight}
                     sets={exercise.sets}
                     reps={exercise.reps}
+                    deleteExercise={deleteExercise}
+                    edit={edit}
                 />
             ))}
         </ScrollView>
